f0d9b4fb48e39bca53475ececcaeaa3f9ee5e324
roc <mario12577@hotmail.com>
2021-03-28 03:28:47 +0700

    takemehome

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+            for (int i = 0;  ; i++)
+            {
+                if (x == y) break;
+                if(x < y)
+                {
+                    x += a;
+                }
+                else
+                {
+                    y += b;
+                }
+                Console.WriteLine(x);
+            }
+
+
+            
+          
+
+        }
+        
+    }
+}
